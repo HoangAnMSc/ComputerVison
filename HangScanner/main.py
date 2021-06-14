7320f661386a54ef7c885cac01b341ee151e1b5a
Vietanh239 <18520253@gm.uit.edu.vn>
2021-06-14 15:13:28 +0700

    updateapp

--- HangScanner/main.py
+++ HangScanner/main.py
@@ -1,58 +1,61 @@
 import cv2
 import numpy as np
-import utils
-from image import Image
-from contours import BiggestContour
-from scanner import Scanner
-import config as cf
+from HangScanner import utils
+from HangScanner.image import Image
+from HangScanner.contours import BiggestContour
+from HangScanner.scanner import Scanner
+import HangScanner.config as cf
 from PIL import Image as image
 
 cap = cv2.VideoCapture(0)
 cap.set(10,160)
- 
-utils.initializeTrackbars()
+
+
 count=0
+def RUNNNNNNN(webcamFeed,path_img):
+    utils.initializeTrackbars()
+    while True:
+        cf.path_img=path_img
+        cf.webcamFeed = webcamFeed
+        thres = utils.valTrackbars()
+        img = Image(cf.widthImg, cf.heightImg, cap, cf.ksize, cf.sigmaX, thres, cf.kernel, cf.path_img, cf.webcamFeed)
+        contour = BiggestContour(img)
+        cv2.drawContours(contour.imgContours, contour.contours, -1, (0, 255, 0), 10) # DRAW ALL DETECTED CONTOURS
 
-while True:
-    thres = utils.valTrackbars()
-    img = Image(cf.widthImg, cf.heightImg, cap, cf.ksize, cf.sigmaX, thres, cf.kernel, cf.path_img, cf.webcamFeed)
-    contour = BiggestContour(img)
-    cv2.drawContours(contour.imgContours, contour.contours, -1, (0, 255, 0), 10) # DRAW ALL DETECTED CONTOURS
-
-    if contour.biggest.size != 0:
-        try:
-            contour.imgBigContour = contour.DrawBiggestContour()
-            scan = Scanner(img, contour)
+        if contour.biggest.size != 0:
+            try:
+                contour.imgBigContour = contour.DrawBiggestContour()
+                scan = Scanner(img, contour)
+                imageArray = ([img.image, img.imageGray, img.imageThreshold, contour.imgContours],
+                            [contour.imgBigContour, scan.imgWarpColored, scan.imgWarpGray, scan.imgAdaptiveThre])
+            except:
+                imageArray = ([img.image, img.imageGray, img.imageThreshold, contour.imgContours],
+                        [img.imageBlank, img.imageBlank, img.imageBlank, img.imageBlank])
+        else:
             imageArray = ([img.image, img.imageGray, img.imageThreshold, contour.imgContours],
-                        [contour.imgBigContour, scan.imgWarpColored, scan.imgWarpGray, scan.imgAdaptiveThre])
-        except:
-            imageArray = ([img.image, img.imageGray, img.imageThreshold, contour.imgContours],
-                      [img.imageBlank, img.imageBlank, img.imageBlank, img.imageBlank])
-    else:
-        imageArray = ([img.image, img.imageGray, img.imageThreshold, contour.imgContours],
-                      [img.imageBlank, img.imageBlank, img.imageBlank, img.imageBlank])
- 
-    # LABELS FOR DISPLAY
-    lables = [["Original","Gray","Threshold","Contours"],
-              ["Biggest Contour","Warp Prespective","Warp Gray","Adaptive Threshold"]]
- 
-    stackedImage = utils.stackImages(imageArray,0.75,lables)
-    cv2.imshow("Result",stackedImage)
- 
-    # SAVE IMAGE WHEN 's' key is pressed
-    pressedKey = cv2.waitKey(1) & 0xFF
-    if pressedKey == ord('s'):
-        cv2.imwrite("Scanned/myImage"+str(count)+".jpg",scan.imgWarpColored)
-        
-        # Convert to PDF
-        image1 = image.open("Scanned/myImage"+str(count)+".jpg")
-        im1 = image1.convert('RGB')
-        im1.save("Scanned/myImage"+str(count)+".pdf")
+                        [img.imageBlank, img.imageBlank, img.imageBlank, img.imageBlank])
+    
+        # LABELS FOR DISPLAY
+        lables = [["Original","Gray","Threshold","Contours"],
+                ["Biggest Contour","Warp Prespective","Warp Gray","Adaptive Threshold"]]
+    
+        stackedImage = utils.stackImages(imageArray,0.75,lables)
+        cv2.imshow("Result",stackedImage)
+    
+        # SAVE IMAGE WHEN 's' key is pressed
+        pressedKey = cv2.waitKey(1) & 0xFF
+        if pressedKey == ord('s'):
+            cv2.imwrite("Scanned/myImage"+str(count)+".jpg",scan.imgWarpColored)
+            
+            # Convert to PDF
+            image1 = image.open("Scanned/myImage"+str(count)+".jpg")
+            im1 = image1.convert('RGB')
+            im1.save("Scanned/myImage"+str(count)+".pdf")
 
-        cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
-                      (1100, 350), (0, 255, 0), cv2.FILLED)
-        cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
-                    cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
-        cv2.imshow('Result', stackedImage)
-        cv2.waitKey(300)
-        count += 1
+            cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
+                        (1100, 350), (0, 255, 0), cv2.FILLED)
+            cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
+                        cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
+            cv2.imshow('Result', stackedImage)
+            cv2.waitKey(300)
+            count += 1
--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
+from HangScanner.main import * 
 from PIL import Image, ImageTk
 class App:
     def __init__(self):
@@ -12,13 +13,23 @@ class App:
         self.Intro.pack()
         self.Button1 = None
         self.my_label =None
+        self.pathname1 =None
+        self.webcamFeed=True
     def files(self):
         self.window.filename = filedialog.askopenfilename(initialdir='image/',title="Select File: ",filetypes=(("jpeg files","*.jpg"),("all files","*.*")))
+        self.pathname1=self.window.filename
+        print(self.pathname1)
+        if self.pathname1:
+            self.webcamFeed=False
+            RUNNNNNNN(self.webcamFeed,self.pathname1)
     def Action_B1(self):
         self.Button1 = Button(self.window, text="Select File",command=self.files)
         self.Button1.pack()
+        print(self.Button1)
         self.my_label= Label(self.window, text="ALL DONE!")
         self.my_label.pack()
+
+        
     def Action(self):
         self.window.mainloop()
 
